a72a6f25293985463f82f929c45fd6358334e5b2
Daniel McClure <daniel@dmsolutionz.co.nz>
2017-07-13 23:33:14 +1000

    masterer

--- solarbank/src/app/my-account/userpage/userpage.component.ts
+++ solarbank/src/app/my-account/userpage/userpage.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserpageComponent implements OnInit {
 
+  public data: any[] = [
+    {
+      kind: 'Consumption', share: 0.2, color: '#5eebf4'
+    },
+    {
+      kind: 'Generation', share: 0.8, color: '#001CB0'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public labelContent(e: any): string {
+    return e.category;
   }
 
 }
