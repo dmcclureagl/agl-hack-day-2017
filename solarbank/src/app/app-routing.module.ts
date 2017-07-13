200af9246462da419def531214605c231fdd4bd7
Daniel McClure <dmcclure@agl.com.au>
2017-07-13 18:52:23 +1000

    asdasdsa

--- solarbank/src/app/app-routing.module.ts
+++ solarbank/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './my-account/home/home.component';
+import { GraphsComponent } from './my-account/graphs/graphs.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'graphs', component: GraphsComponent }
 ];
 
 @NgModule({
--- solarbank/src/app/app.module.ts
+++ solarbank/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -8,20 +9,28 @@ import { HeaderComponent } from './header/header.component';
 // Kendo
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DialogModule } from '@progress/kendo-angular-dialog';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { ChartsModule } from '@progress/kendo-angular-charts';
+import 'hammerjs';
 
 import { HomeComponent } from './my-account/home/home.component';
+import { GraphsComponent } from './my-account/graphs/graphs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    GraphsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     DialogModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    HttpModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
